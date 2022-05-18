88ea032ed5af3b036c09c225d5be4dc7d270523b
George Hodgin <cm14gh@leeds.ac.uk>
2022-05-18 09:33:14 +0100

    Added BaggingRegressor Model to model list

--- CV10_hp_opt.py
+++ CV10_hp_opt.py
@@ -13,64 +13,76 @@ OUTPUT: dataframe containing the best trials in an optuna hyperparameter
 
 @author: gah
 """
-import optuna #package for hyperparameter optimisation
-from optuna.samplers import TPESampler
-import os,re
-import pandas as pd
-import numpy as np
-from sklearn import preprocessing
-from sklearn import ensemble #RF + RF_ET
-from scipy.stats import pearsonr
-from sklearn.model_selection import KFold
-import statistics
+import optuna # Package for hyperparameter optimisation
+from optuna.samplers import TPESampler # Reproducible search space
+import os,re # File path management
+import pandas as pd # Store input and output data
+import numpy as np # For Y_test
+from sklearn import preprocessing # Scaling of training data
+from sklearn import ensemble # RF + RF_ET
+from scipy.stats import pearsonr # for obtaining R2
+from sklearn.model_selection import KFold # CV
+import statistics # Results
 from sklearn.neural_network import MLPRegressor #ANN
 from sklearn.cross_decomposition import PLSRegression #PLS
 
 def main():
     
+    '''Function to perform the Optuna optimisation study on each
+       of the 4 datasets. Handles file import, setting up and
+       execution of the study, and outputs the best results for
+       the 4 scoring metrics (R2, RMSE, N01, N05) in a .csv file
+       named after the training data. "dataname_hp_opt_metrics.csv"'''
     
     dir = os.getcwd() #get current directory to join to files
+    # Import the training data
     AM1 = pd.read_csv(os.path.join(dir,"AM1_full_set_logS.csv"))
     MNDO = pd.read_csv(os.path.join(dir,"MNDO_full_set_logS.csv"))
     PM3 = pd.read_csv(os.path.join(dir,"PM3_full_set_logS.csv"))
     xtb = pd.read_csv(os.path.join(dir,"xtb_full_set_logS.csv"))
+    # List of datasets to iterate through
     data_list = [AM1, MNDO, PM3, xtb]
-    outfile_names = ['AM1_RF_opt_metrics.csv',
-                     'MNDO_RF_opt_metrics.csv',
-                     'PM3_RF_opt_metrics.csv',
-                     'xtb_RF_opt_metrics.csv']
+    # Assign output file names
+    outfile_names = ['AM1_hp_opt_metrics.csv',
+                     'MNDO_hp_opt_metrics.csv',
+                     'PM3_hp_opt_metrics.csv',
+                     'xtb_hp_opt_metrics.csv']
+    # Request number of trials for the study from the user
     n_trials = int(input("Enter number of trials: "))
     
+    # Loop through each dataset and perform the Optuna study
     for dataset, outfile in zip(data_list, outfile_names):
         
-     
-        # define an instance of the Objective class initiated with dataset 
+        # Define an instance of the Objective class initiated with a dataset 
         objective = Objective(dataset)
-        # set random seed so that the results are reproducible
+        # Set random seed so that the results are reproducible
         sampler = TPESampler(seed=10)
-        # create an optuna study to maximize r2, N1, N05 and minimise RMSE
+        # Create an Optuna study to maximize r2, N1, N05 and minimise RMSE
         study = optuna.create_study(directions=["maximize",
                                                 "minimize",
                                                 "maximize",
                                                 "maximize"],
                                     sampler=sampler)
-        # perform optimisation study with n_trials
+        # Perform optimisation study with n_trials
         study.optimize(objective, n_trials=n_trials)
+        # Get the indexes of the best trials for each study
         best = [study.best_trials[i].number for i in range(len(study.best_trials))]
+        # Keep those indexes in the study dataframe
         results = study.trials_dataframe().iloc[best,:]
+        # Output the results to a csv file with the corresponding outfile name
         results.to_csv(outfile, index=False)
     
-#define RMSE
+# Define RMSE
 def rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean())
-#define % within certain range
+# Define % within certain range
 def within_range(list1, list2, range2):
     x=0
     for i in range(len(list2)):
         if (list1[i]-range2)<= list2[i] <= (list1[i]+range2): 
             x+=1
     return((float(x)/(len(list2)))*100)
-#define getting R2 method
+# Define getting R2 method
 def get_R2(R2):
     R2_2=[]
     for i in range(len(R2)):
@@ -94,7 +106,8 @@ class Objective(object):
         # Suggest a ML model to perform 10 fold CV on
         regressor_name = trial.suggest_categorical("regressor",
                                                    ["ANN", "RF",
-                                                    "ET", "PLS"])
+                                                    "ET", "PLS",
+                                                    "BAG"])
         #import Data and randomise
         X = self.data
         X = X.sample(frac=1).reset_index(drop=True)
@@ -164,20 +177,37 @@ class Objective(object):
                 # Train model and get predictions 
                 model.fit(X_train, y_train)
                 preds = model.predict(X_test)
+                
             # PLS
             elif regressor_name == "PLS":
+                # Suggest an integer for number of components
                 n_comps = trial.suggest_int("n_components",
-                                            1, 13, log=False)
+                                            7, 13, log=False) #<7 is rubbish
+                # Setup the model=PLS2
                 model = PLSRegression(n_components=n_comps,
                                      max_iter=1000)
+                # Train model and get predictions
                 model.fit(X_train, y_train)
                 pls2preds = model.predict(X_test)
-                #evaluate model
+                
                 #convert to float (comes in weird type?)
                 preds=[]
                 for i in pls2preds:
                     preds.append(float(i))
                     
+            # Bagging Regressor
+            elif regressor_name == "BAG":
+                # Suggest and integer for number of estimators
+                BAG_trees = trial.suggest_int("n_estimators",
+                                             100, 800, log=False)
+                # Setup the model=BaggingRegressor
+                model = ensemble.BaggingRegressor(n_estimators=BAG_trees,
+                                                  n_jobs=-1)
+                # Train model and get predictions
+                model.fit(X_train, y_train)
+                preds = model.predict(X_test)
+                
+                
             #evaluate model
             r2s.append(pearsonr(preds, y_test))
             RMSEs.append(rmse(preds, y_test))
